b0a5e328c45a83f5add82ef21c8ff158b24374d4
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-05-27 18:19:46 +0200

    Added comments for the ExtendedMath class

--- Math.cs
+++ Math.cs
@@ -6,31 +6,33 @@ namespace Utillities {
     /// </summary>
     public static class ExtendedMath {
         /// <summary>
-        /// 
+        /// Calculates the linear step value based on the input parameters.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <param name="edge0"></param>
-        /// <param name="edge1"></param>
-        /// <returns></returns>
+        /// <param name="x">The input value.</param>
+        /// <param name="n">The power value (default: 1).</param>
+        /// <param name="edge0">The lower edge value (default: 0).</param>
+        /// <param name="edge1">The upper edge value (default: 1.0f).</param>
+        /// <returns>The linear step value.</returns>
         public static float LinearStep(float x, int n = 1, float edge0 = 0, float edge1 = 1.0f) => Clamp((x - edge0) / (edge1 - edge0));
+
         /// <summary>
-        /// 
+        /// Calculates the root step value based on the input parameters.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <param name="edge0"></param>
-        /// <param name="edge1"></param>
-        /// <returns></returns>
+        /// <param name="x">The input value.</param>
+        /// <param name="n">The power value (default: 1).</param>
+        /// <param name="edge0">The lower edge value (default: 0).</param>
+        /// <param name="edge1">The upper edge value (default: 1.0f).</param>
+        /// <returns>The root step value.</returns>
         public static float RootStep(float x, int n = 1, float edge0 = 0, float edge1 = 1.0f) => Clamp(MathF.Sqrt((x - edge0) / (edge1 - edge0)));
+
         /// <summary>
-        /// 
+        /// Calculates the smooth step value based on the input parameters.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <param name="edge0"></param>
-        /// <param name="edge1"></param>
-        /// <returns></returns>
+        /// <param name="x">The input value.</param>
+        /// <param name="n">The power value (default: 1).</param>
+        /// <param name="edge0">The lower edge value (default: 0).</param>
+        /// <param name="edge1">The upper edge value (default: 1.0f).</param>
+        /// <returns>The smooth step value.</returns>
         public static float SmoothStep(float x, int n = 1, float edge0 = 0, float edge1 = 1.0f) {
             x = Clamp((x - edge0) / (edge1 - edge0));
             float result = 0;
@@ -39,20 +41,22 @@ namespace Utillities {
             }
             return result;
         }
+
         /// <summary>
-        /// 
+        /// Clamps the specified value between the given lower and upper limits.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="lowerlimit"></param>
-        /// <param name="upperlimit"></param>
-        /// <returns></returns>
+        /// <param name="x">The value to clamp.</param>
+        /// <param name="lowerlimit">The lower limit (default: 0.0f).</param>
+        /// <param name="upperlimit">The upper limit (default: 1.0f).</param>
+        /// <returns>The clamped value.</returns>
         public static float Clamp(float x, float lowerlimit = 0.0f, float upperlimit = 1.0f) => (x < lowerlimit) ? (lowerlimit) : ((x > upperlimit) ? (upperlimit) : (x));
+
         /// <summary>
-        /// 
+        /// Calculates the value of the Pascal triangle coefficient for the given parameters.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
+        /// <param name="a">The first parameter.</param>
+        /// <param name="b">The second parameter.</param>
+        /// <returns>The Pascal triangle coefficient value.</returns>
         public static float PascalTriangle(int a, int b) {
             float result = 1;
             for (int i = 0; i < b; i++) {
